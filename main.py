35b9b1cb9ba2534910fe6aac3a0fd68c79a9ff02
AlfsAboltins <128421546+221rdb256IngusAlfsAboltins@users.noreply.github.com>
2023-05-18 13:17:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-import heapq
-
 def parallel_processing(n, m, data):
-
     threads = [(0, i) for i in range(n)]
-
-    jobq = [(job, i) for i, job in enumerate(data)]
-
+    jobq = list(enumerate(data))
     output = []
-    
 
     while jobq:
-       
-        job, job_index = heapq.heappop(jobq)
-      
-        start_time, thread_index = heapq.heappop(threads)
-
+        job, job_index = jobq.pop(0)
+        start_time, thread_index = threads.pop(0)
         output.append((thread_index, start_time))
-      
         finish_time = start_time + job
+        threads.append((finish_time, thread_index))
+        threads.sort()
 
-        heapq.heappush(threads, (finish_time, thread_index))
-        
     return output
 
 def main():
-
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
-
     result = parallel_processing(n, m, data)
-    
   
     for thread_index, start_time in result:
         print(thread_index, start_time)
